73b4addf86862012876003d746441d4dec9958ed
Richfatherov <sardoreve@gmail.com>
2022-07-13 14:30:00 +0500

    change_13.07.22__2

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -1,23 +1,22 @@
-import { getDepartmentAndPostTypes } from '../../backend/controller/employeeType/departmentAndPostTypeControlleer.js'
 // let massivofOnePosts = []
 // massivPosts.forEach((item) => {
 //     massivofOnePosts.push(item.post)
 // })
 // massivOFOnePosts
-
-// let posts = {
-//     IT: ['main administrator', 'developer', 'administrator'],
-//     STO: ['nachalnik STO', 'spetsialist STO'],
-//     BUX: ['glavniy BUX', 'pomoshnik BUX'],
-// }
+let posts = {
+    IT: ['main administrator', 'developer', 'administrator'],
+    STO: ['nachalnik STO', 'spetsialist STO'],
+    BUX: ['glavniy BUX', 'pomoshnik BUX'],
+}
 
 let department = document.getElementById('employeeDepartment')
 let post = document.getElementById('employeePost')
 
 department.addEventListener('change', function () {
     console.log('changed')
-    let selected_option = obj[this.value]
-    console.log(getDepartmentAndPostTypes())
+    let selected_option = posts[this.value]
+    // console.log(departmentFromMongo)
+    // console.log(obj)
     while (post.options.length > 0) {
         post.options.remove(0)
     }
--- backend/controller/employeeType/departmentAndPostTypeControlleer.js
+++ backend/controller/employeeType/departmentAndPostTypeControlleer.js
@@ -1,21 +1,17 @@
 import Departments from '../../models/typess/departmentSchema.js'
 import Posts from '../../models/typess/postSchema.js'
-export const obj = {}
+export let name = 'Sardor'
+
 export const getDepartmentAndPostTypes = async (req, res) => {
     try {
         const departmentFromMongo = await Departments.find()
-        const ITdep = await Posts.find({ department: 'IT' })
-        const STOdep = await Posts.find({ department: 'STO' })
-        const BUXdep = await Posts.find({ department: 'BUX' })
-        ;(obj.IT = ITdep), (obj.STO = STOdep), (obj.BUX = BUXdep)
+        // const ITdep = await Posts.find({ department: 'IT' })
+        // const STOdep = await Posts.find({ department: 'STO' })
+        // const BUXdep = await Posts.find({ department: 'BUX' })
+        // ;(obj.IT = ITdep), (obj.STO = STOdep), (obj.BUX = BUXdep)
         res.render('addEmployee', {
             departments: departmentFromMongo,
         })
-        return {
-            ITdep,
-            STOdep,
-            BUXdep,
-        }
     } catch (error) {
         console.log(error)
     }
